247b6404e3272244e0b10fdcf3dd59a2d2eb3f67
MotiMorphoza <mottex@gmail.com>
2026-01-25 04:47:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,39 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("nikud-cache").then(cache =>
-      cache.addAll(["./", "./index.html"])
-    )
+const CACHE_NAME = "nikud-cache-v2";
+const ASSETS = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache =>
+          cache.put(event.request, copy)
+        );
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
